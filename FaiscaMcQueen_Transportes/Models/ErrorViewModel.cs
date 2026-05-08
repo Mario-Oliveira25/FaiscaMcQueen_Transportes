77b74285c5365fc4976080296081a7d551cc3999
Mario Oliveira <Mario.Oliveira.T0133601@edu.atec.pt>
2026-05-08 19:38:03 +0100

    Add project files.

---
+++ FaiscaMcQueen_Transportes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FaiscaMcQueen_Transportes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
