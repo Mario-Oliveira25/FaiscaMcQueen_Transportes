965ab303852de97692f3a30f5e6a9453d9c7e85c
Mario Oliveira <Mario.Oliveira.T0133601@edu.atec.pt>
2026-05-08 21:07:17 +0100

    Classes Ativo e Tecnico Closes #1

---
+++ FaiscaMcQueen_Transportes/Data/FaiscaMcQueen/Tecnico.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FaiscaMcQueen_Transportes.Data.FaiscaMcQueen
+{
+    public class Tecnico
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O NIF é obrigatório")]
+        [RegularExpression(@"^[1235689]\d{8}$", ErrorMessage = "O NIF tem de ter 9 números e começar por um dígito válido.")]
+        public string Nif { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DataNascimento { get; set; }
+
+        public string Especialidade { get; set; } = string.Empty;
+
+        public ICollection<Intervencao> Intervencoes { get; set; } = new List<Intervencao>();
+    }
+}
