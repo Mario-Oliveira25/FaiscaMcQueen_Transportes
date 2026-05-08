c165c92118bfdf89d1842a07bf9ae74dfe74eb9b
Nuno Queirós <267765083+nunoQueirosCode@users.noreply.github.com>
2026-05-08 20:56:28 +0100

    Classe Intervencao feita - closes #2

---
+++ FaiscaMcQueen_Transportes/Data/Intervencao.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
+namespace FaiscaMcQueen_Transportes.Data
+{
+    public class Intervencao
+    {
+        public enum Estado
+        {
+            Pendente,
+            EmCurso,
+            Concluido
+        }
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public Estado estado { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+        [Required]
+        public string Descricao { get; set; } = string.Empty;
+        [Required]
+        public Guid AtivoId { get; set; }
+        public Ativo Ativo { get; set; }
+        [Required]
+        public Guid TecnicoId { get; set; }
+        public Tecnico Tecnico { get; set; }
+    }
+}
