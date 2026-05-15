50b09b79598b0cf45191f22f6b2cbfa8ebb35877
Nuno Queirós <267765083+nunoQueirosCode@users.noreply.github.com>
2026-05-15 19:35:35 +0100

    Minor changes

--- FaiscaMcQueen_Transportes/Data/FaiscaMcQueen/Intervencao.cs
+++ FaiscaMcQueen_Transportes/Data/FaiscaMcQueen/Intervencao.cs
@@ -8,7 +8,9 @@ namespace FaiscaMcQueen_Transportes.Data.FaiscaMcQueen
         public enum estado
         {
             Pendente,
+            [Display(Name = "Em Curso")]
             EmCurso,
+            [Display(Name = "Concluído")]
             Concluido
         }
         [Key]
